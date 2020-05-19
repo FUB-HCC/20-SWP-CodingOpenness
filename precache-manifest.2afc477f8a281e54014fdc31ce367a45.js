a48cbc36058514f353920efe686b17307d78d857
adrianapintod <luzadriana.pin@gmail.com>
2020-05-19 17:56:25 +0200

    update contribution requirements

---
+++ precache-manifest.2afc477f8a281e54014fdc31ce367a45.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1245d7754e105811cfb7623f310bcd07",
+    "url": "/20-SWP-CodingOpenness/index.html"
+  },
+  {
+    "revision": "d7cc80c720be39868ef7",
+    "url": "/20-SWP-CodingOpenness/static/css/2.75c2aa79.chunk.css"
+  },
+  {
+    "revision": "50cd64132dbc923419b4",
+    "url": "/20-SWP-CodingOpenness/static/css/main.ec56a646.chunk.css"
+  },
+  {
+    "revision": "d7cc80c720be39868ef7",
+    "url": "/20-SWP-CodingOpenness/static/js/2.eb5d303a.chunk.js"
+  },
+  {
+    "revision": "e18637604789af6a40503615de5bd745",
+    "url": "/20-SWP-CodingOpenness/static/js/2.eb5d303a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50cd64132dbc923419b4",
+    "url": "/20-SWP-CodingOpenness/static/js/main.19819570.chunk.js"
+  },
+  {
+    "revision": "fc223838ad78f9650572",
+    "url": "/20-SWP-CodingOpenness/static/js/runtime-main.e005725f.js"
+  },
+  {
+    "revision": "a0f38bb70c408f82d56108c47978274c",
+    "url": "/20-SWP-CodingOpenness/static/media/fu-logo.a0f38bb7.png"
+  },
+  {
+    "revision": "9407832cc361117fb3da5e61f8881ce1",
+    "url": "/20-SWP-CodingOpenness/static/media/github.9407832c.svg"
+  },
+  {
+    "revision": "9f9845e81f2761fb62e4176597cc628c",
+    "url": "/20-SWP-CodingOpenness/static/media/hcc-logo.9f9845e8.png"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/20-SWP-CodingOpenness/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/20-SWP-CodingOpenness/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/20-SWP-CodingOpenness/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/20-SWP-CodingOpenness/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/20-SWP-CodingOpenness/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/20-SWP-CodingOpenness/static/media/icons-20.cef8cdbb.woff"
+  }
+]);
