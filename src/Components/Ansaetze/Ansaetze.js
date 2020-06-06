6b279e62c82649dbadafdd621a732fe05e655f12
tratond01 <32215264+tratond01@users.noreply.github.com>
2020-06-06 14:31:43 +0200

    Ansaetze sticky navbar fix and Kontakt change of origin email address (#96)
    * Ansaetze sticky navbar fix and Kontakt change of origin email addres
    
    * arrow up removed since navbar's now sticky
    
    Co-authored-by: Hod04 <omerhod@gmail.com>

--- src/Components/Ansaetze/Ansaetze.js
+++ src/Components/Ansaetze/Ansaetze.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Ansaetze.css";
-import { Button } from "@blueprintjs/core";
 
 class Ansaetze extends React.Component {
   componentDidMount() {
@@ -58,13 +57,6 @@ class Ansaetze extends React.Component {
               />
             </div>
           </div>
-
-          <Button
-            style={{ marginLeft: 50 }}
-            icon={"arrow-up"}
-            minimal
-            onClick={() => window.scrollTo(0, 0)}
-          />
         </div>
 
         <div className="DP-3T" id="DP-3T">
@@ -116,13 +108,6 @@ class Ansaetze extends React.Component {
               />
             </div>
           </div>
-
-          <Button
-            style={{ marginLeft: 50 }}
-            icon={"arrow-up"}
-            minimal
-            onClick={() => window.scrollTo(0, 0)}
-          />
         </div>
 
         <div className="DCTS" id="DCTS">
@@ -144,13 +129,6 @@ class Ansaetze extends React.Component {
               />
             </div>
           </div>
-
-          <Button
-            style={{ marginLeft: 50 }}
-            icon={"arrow-up"}
-            minimal
-            onClick={() => window.scrollTo(0, 0)}
-          />
         </div>
       </div>
     );
