052423e3803fcd51cacf181c4f23fbb0b2c16141
oussab <oussama.bouanani@tutanota.com>
2020-06-18 20:19:09 +0200

    Added app store and play store download links and images

--- src/Components/CoronaWarnApp/CoronaWarnApp.js
+++ src/Components/CoronaWarnApp/CoronaWarnApp.js
@@ -18,6 +18,22 @@ class CoronaWarnApp extends React.Component {
           alt="corona_warn_app_title_img"
         />
 
+        <div className="download-img-row">
+          <a href="https://play.google.com/store/apps/details?id=de.rki.coronawarnapp">
+            <img
+              className="download-img-column-left"
+              src={require("../../Assets/play_store_img.png")}
+              alt="corona_warn_app_play_store"
+            />
+          </a>
+          <a href="https://apps.apple.com/de/app/corona-warn-app/id1512595757">
+            <img
+              className="download-img-column-right"
+              src={require("../../Assets/app_store_img.png")}
+              alt="corona_warn_app_app_store"
+            />
+          </a>
+        </div>
 
       </div>
     </div>
