d130e6bcc8c77bb25571cd72afd21e23926594e7
Dhih <tratond01@gmail.com>
2020-06-21 22:25:41 +0200

    init commit of contribution page and restructed about page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -24,19 +24,30 @@ class About extends React.Component {
               >
                 Wer sind wir
               </div>
+            </div>
+
+            <div className={"about-links"} style={{ fontWeight: 10 }}>
               <div className={"abschnitt"}>
-                {`Wir sind Studenten am Institut für Informatik an der Freien Universität Berlin und beschäftigen uns im 
-            Modul Softwareprojekt - Coding Openness mit der deutschen Corona App.`}
+                {`Wir sind Studenten am Institut für Informatik an der Freien Universität 
+                Berlin und beschäftigen uns im Modul Softwareprojekt - Coding Openness 
+                mit der deutschen Corona App.`}
               </div>
+              
               <div className={"abschnitt"}>
-                {`Letztlich besteht die Hoffnung, dass wir die Möglichkeit erhalten, die deutsche Corona App zu installieren und 
-              zu erweitern. Zur Umsetzung haben wir uns in Folgende Teams aufgeteilt.`}
+                {`Die Beiträge jeder Person werden aufgelistet und jeweils  
+                die persönliche Github-Seite verlinkt.`}
               </div>
-              <div className={"abschnitt"} style={{ fontWeight: 10 }}>
+
+              <div className={"abschnitt"}>
+                {`Letztlich besteht die Vorhaben, die deutsche Corona App zu installieren 
+                und zu erweitern.`}
+              </div>
+              <br />
+
                 <strong style={{ textDecoration: "underline" }}>
                   {"Links:"}
                 </strong>
-                <br />
+                <br /><br />
                 <div>
                   <a
                     style={{ margin: 5, color: "#6b9e1f" }}
@@ -70,68 +81,274 @@ class About extends React.Component {
                         color: "black",
                         paddingLeft: 2,
                       }}
-                    >
-                      {
-                        "Interview with Prof. Claudia Müller Birn on the tracing app"
-                      }
+                    >{"Interview with Prof. Claudia Müller Birn on the tracing app"}
                     </span>
                   </a>
                 </div>
-              </div>
-            </div>
-            <div style={{ display: "flex" }}>
-              <div className={"team"}>
-                <div className={"team-title"}>{"Team Technik"}</div>
-                <ul>
-                  {this.getGroupMemberList([
-                    "Julius Brose",
-                    "Long Dang Pham Hoang",
-                    "Dennis Nikolaus Natusch",
-                    "Johannes Brose",
-                  ])}
-                </ul>
-                <div className={"team-title"}>{"Team Organisation"}</div>
-                <ul>
-                  {this.getGroupMemberList([
-                    "Keno Wilhelm Budde",
-                    "Adriana Pinto Diaz Luz",
-                  ])}
-                </ul>
-              </div>
-              <div className={"team"}>
-                <div className={"team-title"}>{"Team Webseite"}</div>
-                <ul>
-                  {this.getGroupMemberList([
-                    "Di Wang",
-                    "Oussama Bouanani",
-                    "Son Tung Duong",
-                    "Omer Hod",
-                  ])}
-                </ul>
-                <div className={"team-title"}>{"Team Fragebogen"}</div>
-                <ul>
-                  {this.getGroupMemberList([
-                    "Ingrid Tchilibou",
-                    "Felix Sekul",
-                    "David Paulini",
-                    "Torben Knaak",
-                  ])}
-                </ul>
-              </div>
-              <div className={"team"}>
-                <div className={"team-title"}>{"Team Vergleich"}</div>
-                <ul>
-                  {this.getGroupMemberList([
-                    "Claas Fandre",
-                    "Linus Helfmann",
-                    "Bernd Sahre",
-                    "Viktoriya Kraleva",
-                    "Julia Zimmermann",
-                  ])}
-                </ul>
-              </div>
             </div>
           </div>
+
+          <div class="about-list"> 
+            <p style={{textAlign: 'center'}}>Namen sind alphabetisch nach Nachnamen sortiert.</p>          
+                      
+            <div class="member-box">
+              <span style={{fontWeight: 'bold'}}>
+                <a href="https://github.com/OussamaBouanani"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >Oussama Bouanani </a>
+                </span>
+              <p>
+                Team Webseite <br />
+                DP-3T Ansatz
+              </p>
+            </div>
+
+            <div class="member-box">
+              <span style={{fontWeight: 'bold'}}>
+                <a href="https://github.com/JohBrose"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >Johannes Brose </a>
+              </span>
+              <p>
+                Team Technik <br />
+                PEPP-PT Ansatz
+              </p>
+            </div>
+
+            <div class="member-box">
+              <span style={{fontWeight: 'bold'}}>
+                <a href="https://github.com/JuliusBro"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >Julius Brose </a>
+                </span>
+              <p>
+                Team Technik<br />
+                DCTS Ansatz (Leiter)<br />
+                Corona Warn App: Risk
+              </p>
+            </div>
+
+            <div class="member-box">
+              <span style={{fontWeight: 'bold'}}>
+                <a href="https://github.com/Clearwood"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >Keno Wilhelm Budde </a>
+              </span>
+              <p>
+                Team Organisation<br />
+                DP-3T Ansatz      
+              </p>
+            </div>
+
+            <div class="member-box">
+              <span style={{fontWeight: 'bold'}}>
+                <a href="https://github.com/sontungg"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >Son Tung Duong </a>
+                </span>
+              <p>
+                Team Webseite<br />
+                DP-3T Ansatz
+              </p>
+            </div>
+
+            <div class="member-box">
+              <span style={{fontWeight: 'bold'}}>
+                <a href="https://github.com/erdanf"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >Claas Fandre </a>
+                </span>
+              <p>
+                Team Vergleich<br />
+                DCTS Ansatz<br />
+                Corona Warn App: Data Privacy
+              </p>
+            </div>          
+
+            <div class="member-box">
+              <span style={{fontWeight: 'bold'}}>
+                <a href="https://github.com/linuxhelf"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >Linus Helfmann </a>
+              </span>
+              <p>
+                Team Vergleich<br />
+                PEPP-PT Ansatz (Leiter)<br />
+                Corona Warn App: Data Privacy
+              </p>
+            </div>           
+
+            <div class="member-box">
+              <span style={{fontWeight: 'bold'}}>
+                <a href="https://github.com/DPHLong"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >Long Dang Pham Hoang </a>
+              </span>
+              <p>
+                Team Technik<br />
+                PEPP-PT Ansatz<br />
+                Corona Warn App: Risk
+              </p>
+            </div>
+
+            <div class="member-box">
+              <span style={{fontWeight: 'bold'}}>
+                <a href="https://github.com/Hod04"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >Omer Hod </a>
+              </span>
+              <p>
+                Team Webseite<br />
+                DP-3T Ansatz
+              </p>
+            </div>
+
+            <div class="member-box">
+              <span style={{fontWeight: 'bold'}}>
+                <a href="https://github.com/eagle-seagull"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >Torben Knaak </a>
+                </span>
+              <p>
+                Team Fragebogen<br />
+                DCTS Ansatz
+              </p>
+            </div>          
+
+            <div class="member-box">
+              <span style={{fontWeight: 'bold'}}>
+                <a href="https://github.com/kraleva"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >Viktoriya Kraleva </a>
+              </span>
+              <p>
+                Team Vergleich<br />
+                PEPP-PT Ansatz<br />
+                Corona Warn App: UI Design
+              </p>
+            </div>          
+
+            <div class="member-box">
+              <span style={{fontWeight: 'bold'}}>
+                <a href="https://github.com/adrianapintod"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >Adriana Pinto Diaz Luz </a>
+              </span>
+              <p>
+                Team Organisation<br />
+                DP-3T Ansatz
+              </p>
+            </div>            
+
+            <div class="member-box">
+              <span style={{fontWeight: 'bold'}}>
+                <a href="https://github.com/dennisn00"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >Dennis Nikolaus Natusch </a>
+              </span>
+              <p>
+                Team Technik<br />
+                DP-3T Ansatz
+              </p>
+            </div>
+
+            <div class="member-box">
+              <span style={{fontWeight: 'bold'}}>
+                <a href="https://github.com/pada1015"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >David Paulini </a>
+              </span>
+              <p>
+                Team Fragebogen<br />
+                PEPP-PT Ansatz
+              </p>
+            </div>
+
+            <div class="member-box">
+              <span style={{fontWeight: 'bold'}}>
+                <a href="https://github.com/bernd961"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >Bernd Sahre </a>
+                </span>
+              <p>
+                Team Vergleich<br />
+                DCTS Ansatz
+              </p>
+            </div>          
+
+            <div class="member-box">
+              <span style={{fontWeight: 'bold'}}>
+                <a href="https://github.com/molpremotone"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >Felix Sekul </a>
+                </span>
+              <p>
+                Team Fragebogen<br />
+                PEPP-PT Ansatz<br />
+                Corona Warn App: Exposure
+              </p>
+            </div>          
+
+            <div class="member-box">
+              <span style={{fontWeight: 'bold'}}>
+                <a href="https://github.com/gancia-kiss"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >Ingrid Tchilibou </a>
+              </span>
+              <p>
+                Team Fragebogen<br />
+                PEPP-PT Ansatz<br />
+                Corona Warn App: Exposure
+              </p>
+            </div>
+                      
+            <div class="member-box">
+              <span style={{fontWeight: 'bold'}}>
+                <a href="https://github.com/tratond01"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >Di Wang </a>
+              </span>
+              <p>
+                Team Webseite<br />
+                PEPP-PT Ansatz
+              </p>
+            </div>          
+              
+            <div class="member-box">
+              <span style={{fontWeight: 'bold'}}>
+                <a href="https://github.com/julizet"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >Julia Zimmermann </a>
+                </span>
+              <p>
+                Team Vergleich<br />
+                DP-3T Ansatz (Leiterin)<br />
+                Corona Warn App: Data Privacy
+              </p>
+            </div>  
+             
+          </div>   
+          <div class="end">
+          </div> 
         </div>
       </>
     );
--- src/Components/TeamContribution/TeamContribution.js
+++ src/Components/TeamContribution/TeamContribution.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./TeamContribution.css";
+import { Link } from "react-router-dom";
 
 class TeamContribution extends React.Component {
   componentDidMount() {
@@ -8,7 +9,67 @@ class TeamContribution extends React.Component {
     }
   }
   render() {
-    return <>hey</>;
+    return (
+      <>
+        <div  className={"contribution-container"}>
+          <div className={"contribution-intro"}>
+            <div style={{ fontSize: 50, fontWeight: 800 }}>
+                  <span style={{ color: "#6b9e1f" }}>Beiträge</span> zur 
+                  Corona-Warn-App
+            </div>
+            <div>
+              <p>
+                    Beiträge der Lehrveranstaltungsteilnehmer zur Corona-Warn-App 
+                    sind hier dokumentiert.
+              </p>
+              <p>
+                Für die entsprechende Github-Seite folgen Sie bitte den Links.
+                Weitere Informationen zur entsprechenden Person finden Sie unter
+                 <Link className={"contri-about-link"} to={"/about"}> Über Uns.</Link>
+              </p>
+            </div>
+          </div>
+
+          <div className={"contribution-list"}>
+            
+            <div class="boxed">
+              <div class="boxhead">
+                <p>
+                  <a
+                    href="https://github.com/corona-warn-app/cwa-app-android/issues/381"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Contribution to the Packages Wiki Page
+                  </a> <br/> by Julius Brose
+                </p>
+              </div>
+              <div class="boxbody"> 
+                <p>Makes it easier for people new to the project/code to find the 
+                  critical files quickly and gain an understanding of how things 
+                  work.</p>
+                <p>
+                  <a
+                      href="https://github.com/corona-warn-app/cwa-app-android/wiki/5-Packages"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                  >
+                      Updated package wiki.
+                  </a>
+                  <span style={{float: 'right'}}>
+                    12-06-2020
+                  </span>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="end">
+          </div>
+        </div>
+      </>
+    );
   }
 }
+
 export default TeamContribution;
